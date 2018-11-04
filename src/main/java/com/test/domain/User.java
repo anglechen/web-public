dc95d2a7b6b5236c2479e4497ea3925515a922f2
aubrey <676719570@qq.com>
2018-11-04 11:12:23 +0800

    添加实体类

---
+++ src/main/java/com/test/domain/User.java
@@ -0,0 +1,26 @@
+package com.test.domain;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class User {
+	
+  private Integer id;
+  private String account;
+  private String name;
+  private String password;
+  private Integer age;
+  private String phoneNumber;
+  private String registerDate;
+  private String mail;
+  private Integer sex;
+  private Integer status;
+  private Date creTime;
+  private String creUser;
+  private Date modTime;
+  private String modUser;
+  private Integer delFlag;
+
+}
