6901416bba13157177b69ce9f2ea7cfd600fe4a8
aubrey <676719570@qq.com>
2018-11-10 14:44:25 +0800

    添加servlet相关代码

--- src/main/java/com/test/domain/User.java
+++ src/main/java/com/test/domain/User.java
@@ -30,4 +30,97 @@ public class User {
   private Date modTime;
   private String modUser;
   private Integer delFlag;
+public Integer getId() {
+	return id;
+}
+public void setId(Integer id) {
+	this.id = id;
+}
+public String getAccount() {
+	return account;
+}
+public void setAccount(String account) {
+	this.account = account;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public Integer getAge() {
+	return age;
+}
+public void setAge(Integer age) {
+	this.age = age;
+}
+public String getPhoneNumber() {
+	return phoneNumber;
+}
+public void setPhoneNumber(String phoneNumber) {
+	this.phoneNumber = phoneNumber;
+}
+public String getRegisterDate() {
+	return registerDate;
+}
+public void setRegisterDate(String registerDate) {
+	this.registerDate = registerDate;
+}
+public String getMail() {
+	return mail;
+}
+public void setMail(String mail) {
+	this.mail = mail;
+}
+public Integer getSex() {
+	return sex;
+}
+public void setSex(Integer sex) {
+	this.sex = sex;
+}
+public Integer getStatus() {
+	return status;
+}
+public void setStatus(Integer status) {
+	this.status = status;
+}
+public Date getCreTime() {
+	return creTime;
+}
+public void setCreTime(Date creTime) {
+	this.creTime = creTime;
+}
+public String getCreUser() {
+	return creUser;
+}
+public void setCreUser(String creUser) {
+	this.creUser = creUser;
+}
+public Date getModTime() {
+	return modTime;
+}
+public void setModTime(Date modTime) {
+	this.modTime = modTime;
+}
+public String getModUser() {
+	return modUser;
+}
+public void setModUser(String modUser) {
+	this.modUser = modUser;
+}
+public Integer getDelFlag() {
+	return delFlag;
+}
+public void setDelFlag(Integer delFlag) {
+	this.delFlag = delFlag;
+}
+  
+  
+  
  }
--- src/main/java/com/test/servlet/UserAddServlet.java
+++ src/main/java/com/test/servlet/UserAddServlet.java
@@ -2,7 +2,9 @@ package com.test.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,28 +12,42 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.beanutils.BeanUtils;
+
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.test.domain.User;
 import com.test.service.UserService;
-import com.test.service.annotation.Test;
 import com.test.service.impl.UserServiceImpl;
 
-@WebServlet("/users/list")
-public class UserServlet extends HttpServlet {
-
+@WebServlet("/users/add")
+public class UserAddServlet extends HttpServlet{
+	
 	private UserService userService = new UserServiceImpl();
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setCharacterEncoding("utf-8");
 		PrintWriter print = resp.getWriter();
-		List<User> users = userService.query(new User());
+		//一个个参数获取
+		String account = req.getParameter("account");
+		String password = req.getParameter("password");
+		//获取请求对象中的所有参数
+		Map parameters = req.getParameterMap();
+		User user = new User();
+		try {
+			BeanUtils.populate(user, parameters);
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
+//		JSONObject.toJavaObject((JSONObject)JSONObject.toJSON(parameters), User.class);
+		userService.add(user);
+		
 		JSONObject jsonObject = new JSONObject();
-		jsonObject.put("count", users.size());
-		jsonObject.put("data", users);
+		jsonObject.put("result", "success");
 		resp.setContentType("application/json,charset=utf-8");
 		print.write(jsonObject.toJSONString());
 	}
-	
-	
 }
